58f23c030c0ac884a0f81b083807a7027b54eb76
marcxargay <marc.xargay@sunwebgroup.com>
2025-01-24 10:53:31 +0100

    fix build

--- src/app/play/hooks.tsx
+++ src/app/play/hooks.tsx
@@ -1,13 +1,12 @@
 import {useGameStore} from "@/app/store";
 import allData from '@/../public/data.json'
-import { Value} from "@/types";
 
 export const useValues = () => {
     const {selectedCategories} = useGameStore()
 
     const values = Object.entries(allData)
         .filter(([key]) => selectedCategories.includes(key))
-        .flatMap(([_, value]) => value.values)
+        .flatMap(([, value]) => value.values)
 
     return {
         values
